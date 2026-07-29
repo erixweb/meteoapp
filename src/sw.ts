2203debf3e62c949deb84bbb4bc036ac6d90cfca
erixweb <erikmorenodev@gmail.com>
2026-07-29 17:25:38 +0200

    create: sw.ts

---
+++ src/sw.ts
@@ -0,0 +1,4 @@
+import { precacheAndRoute } from 'workbox-precaching';
+
+// $Workbox$sw: this is the placeholder for precache manifest injected at build time.
+precacheAndRoute(self.__WB_MANIFEST);
